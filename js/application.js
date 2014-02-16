348ee6e26e88ca8c174d4ee87531c703da0f6b7b
Graeme Collins <graeme.collins.0@gmail.com>
2014-02-15 20:32:25 -0600

    Added FPS counter.

--- js/application.js
+++ js/application.js
@@ -7,6 +7,9 @@ var renderer;
 var world;
 var worldRenderer;
 var inputManager;
+var fps;
+var fpsText;
+var lastLoop = new Date();
 
 // Initialize
 function initialize()
@@ -34,8 +37,30 @@ function initialize()
     inputManager.onKeyUp(e.keyCode);
   };
   
+  // FPS
+  fps = [60, 60, 60, 60, 60, 60, 60, 60, 60, 60];
+  fpsText = new PIXI.Text("...", { font: "bold 20pt Trebuchet MS", fill: "black" });
+  stage.addChild(fpsText);
+  
   // Start main loop
   requestAnimFrame(loop);
+}
+
+// Update fps
+function updateFps()
+{
+  var thisLoop = new Date();
+  var averageFps = 0;
+
+  // FPS
+  fps.shift();
+  fps.push(1000 / (thisLoop.getTime() - lastLoop.getTime()));
+  for (var i = 0; i < fps.length; i++)
+  {
+      averageFps += fps[i] / fps.length;
+  }
+  lastLoop = thisLoop;
+  fpsText.setText("FPS: " + Math.floor(averageFps));
 }
 
 // Handle input
@@ -65,6 +90,7 @@ function handleInput()
 function loop()
 { 
   // Update
+  updateFps();
   handleInput();
   
   // Draw
