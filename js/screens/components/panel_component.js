b7dc9d242fd5e7f9164d01d69864e106fe66c5bb
Graeme Collins <graeme.collins.0@gmail.com>
2014-03-04 04:46:12 -0600

    Panel code works.

---
+++ js/screens/components/panel_component.js
@@ -0,0 +1,80 @@
+var PanelComponent = function(options)
+{
+  this.base = PIXI.DisplayObjectContainer;
+  this.base();
+  
+  options = options || {};
+  options.x = options.x || 0;
+  options.y = options.y || 0;
+  options.width = options.width || 640;
+  options.height = options.height || 320;
+  options.centerX = options.centerX || false;
+  options.centerY = options.centerY || false;
+  
+  this.width = options.width;
+  this.height = options.height;
+  this.position.x = options.x;
+  this.position.y = options.y;
+  
+  this.buildPanel();
+};
+
+PanelComponent.prototype = new PIXI.DisplayObjectContainer;
+
+PanelComponent.prototype.buildPanel = function()
+{
+  var topLeft = new PIXI.Sprite(PIXI.Texture.fromImage(assetPathManager.assetPaths.ui.panelCorners[0]));
+  var topRight = new PIXI.Sprite(PIXI.Texture.fromImage(assetPathManager.assetPaths.ui.panelCorners[1]));
+  var bottomRight = new PIXI.Sprite(PIXI.Texture.fromImage(assetPathManager.assetPaths.ui.panelCorners[2]));
+  var bottomLeft = new PIXI.Sprite(PIXI.Texture.fromImage(assetPathManager.assetPaths.ui.panelCorners[3]));
+  var top = new PIXI.Sprite(PIXI.Texture.fromImage(assetPathManager.assetPaths.ui.panelSides[0]));
+  var right = new PIXI.Sprite(PIXI.Texture.fromImage(assetPathManager.assetPaths.ui.panelSides[1]));
+  var bottom = new PIXI.Sprite(PIXI.Texture.fromImage(assetPathManager.assetPaths.ui.panelSides[2]));
+  var left = new PIXI.Sprite(PIXI.Texture.fromImage(assetPathManager.assetPaths.ui.panelSides[3]));
+  var bg = new PIXI.Sprite(PIXI.Texture.fromImage(assetPathManager.assetPaths.ui.panelBg));
+  
+  // Corners
+  topLeft.position.x = 0;
+  topLeft.position.y = 0;
+  this.addChild(topLeft);
+  
+  topRight.position.x = this.width - topRight.width;
+  topRight.position.y = 0;
+  this.addChild(topRight);
+  
+  bottomRight.position.x = this.width - bottomRight.width;
+  bottomRight.position.y = this.height - bottomRight.height;
+  this.addChild(bottomRight);
+  
+  bottomLeft.position.x = 0;
+  bottomLeft.position.y = this.height - bottomLeft.height;
+  this.addChild(bottomLeft);
+  
+  // Sides
+  top.position.x = topLeft.width;
+  top.position.y = 0;
+  top.width = this.width - (topLeft.width + topRight.width);
+  this.addChild(top);
+  
+  right.position.x = this.width - right.width;
+  right.position.y = topRight.height;
+  right.height = this.height - (topRight.height + bottomRight.height);
+  this.addChild(right);
+  
+  bottom.position.x = bottomLeft.width;
+  bottom.position.y = this.height - bottom.height;
+  bottom.width = this.width - (bottomLeft.width + bottomRight.width);
+  this.addChild(bottom);
+  
+  left.position.x = 0;
+  left.position.y = topLeft.height;
+  left.height = this.height - (topLeft.height + bottomLeft.height);
+  this.addChild(left);
+  
+  // Background
+  bg.position.x = topLeft.width;
+  bg.position.y = topLeft.height;
+  bg.width = this.width - (topLeft.width + topRight.width);
+  bg.height = this.height - (topLeft.height + bottomLeft.height);
+  this.addChild(bg);
+};
--- js/screens/world_map_screen.js
+++ js/screens/world_map_screen.js
@@ -2,18 +2,26 @@ var WorldMapScreen = function()
 {
   this.type = ScreenType.WorldMap;
   this.worldRenderer = new WorldRenderer();
+  this.bottomBar = new PanelComponent({
+    x: -8,
+    y: game.containerHeight - 28,
+    width: game.containerWidth + 16,
+    height: 34
+  });
 };
 
 WorldMapScreen.prototype.onAddScreen = function()
 {
   game.stage.addChild(this.worldRenderer.container);
   game.stage.addChild(this.worldRenderer.camera);
+  game.stage.addChild(this.bottomBar);
 };
 
 WorldMapScreen.prototype.onRemoveScreen = function()
 {
   game.stage.removeChild(this.worldRenderer.container);
   game.stage.removeChild(this.worldRenderer.camera);
+  game.stage.removeChild(this.bottomBar);
 };
 
 WorldMapScreen.prototype.update = function()
